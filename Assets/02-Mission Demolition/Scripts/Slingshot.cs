e516170e78c89b6cd404598540df9f6039ccd21a
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2022-09-21 15:15:33 -0400

    added goal and projectile line

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -6,6 +6,8 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
+
     // fields set in the Unity Inspector pane
 
     [Header("Set in Inspector")]                                            
@@ -31,8 +33,23 @@ public class Slingshot : MonoBehaviour
     private Rigidbody projectileRigidbody;
 
 
+    static public Vector3 LAUNCH_POS
+    {                                        
+
+        get
+        {
+
+            if (S == null) return Vector3.zero;
+
+            return S.launchPos;
+
+        }
+
+    }
+
     void Awake()
     {
+        S = this;
 
         Transform launchPointTrans = transform.Find("LaunchPoint");              
 
