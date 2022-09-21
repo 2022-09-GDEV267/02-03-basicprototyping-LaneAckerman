a477b8aef349f0a5c954025f92b0548a46a0f789
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2022-09-21 12:46:08 -0400

    setup follower camera

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -147,6 +147,8 @@ public class Slingshot : MonoBehaviour
 
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
 
+            FollowCam.POI = projectile;
+
             projectile = null;
 
         }
